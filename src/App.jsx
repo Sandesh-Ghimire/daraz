230069dd08a62c11d9574b4c8bc87e522635e53f
sandesh-ghimire <ghimiresandesh45@gmail.com>
2024-09-12 21:35:05 +0530

    working on crud operation.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,46 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, {jobLoader} from './pages/JobPage';
+import Addjob from './pages/Addjob';
+import EditjobPage from './pages/EditjobPage';
+
+const App = () => {
+//add new job  
+const addjob = async(newjob)=> {
+const res= await  fetch('/api/jobs',{
+method: 'POST',
+headers:{
+  'Content-Type':application/json
+},
+body: JSON.stringify(newjob),
+    }
+  );
+  return;
+};
+
+//deletejob
+const deletejob= async (id)=>
+{
+  const res= await  fetch(`/api/jobs/${id}`,{
+    method: 'DELETE',
+  });
+      return;
+}
+
+const updateJob= async(job)=>
+{
+  const res= await  fetch(`/api/jobs/${id}`,{
+    method: 'PUT',
+  
+  headers:{
+    'Content-Type':application/json
+  },
+  body: JSON.stringify(job),
+      }
+    );
+      return;
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,16 +58,19 @@ const router = createBrowserRouter(
   <Route index element={<HomePage/>}/>
   <Route path='/jobs' element={<JobsPage/>}/>
   <Route path='*' element={<NotFoundPage/>}/>
-  <Route path='/jobs/:id' element={<JobPage/>}/>
+  <Route path='/jobs/:id' element={<JobPage deletejob={
+    deletejob}/>} loader={jobLoader}/>
+     <Route path='/edit-job/:id' element={<EditjobPage updateJobSumbit={updateJob}/>} loader={jobLoader}/>
+  <Route path='/add-job' element={<Addjob addJobSumbit={addjob}/>}/>
 
 </Route>
 )
 );
 
-
-const App = () => {
   return (
+
    <RouterProvider router={router}/>
+
   );
 };
 
